f1003476cec86d9afb443af734e030b936a86de3
NineMan <NineMan@github.com>
2020-10-14 16:06:25 +0400

    change table in database

--- aiohttp_polls/views.py
+++ aiohttp_polls/views.py
@@ -6,15 +6,13 @@ import logging
 async def mysql_get_handler(request):
     async with request.app['db'].acquire() as connection:
         async with connection.cursor() as cursor:
-            query = "SELECT email, password FROM users"
+            query = "SELECT id, product_name, description, value FROM neovox_products"
             await cursor.execute(query)
             records = await cursor.fetchall()
 
             answer = ''
             for record in records:
-                for field in record:
-                    answer += field + ' '
-                answer += '\n'
+                answer = answer + ' : '.join(map(str, record)) + '\n'
 
     return web.Response(text=answer)
 
