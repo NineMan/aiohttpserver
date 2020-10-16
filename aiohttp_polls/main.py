81eed7ec5bb686c917d02d79a7889b0add5483b2
NineMan <NineMan@github.com>
2020-10-16 04:46:28 +0400

    Add logging partial

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from aiohttp import web_app
 import logging
 
 from settings import config
@@ -7,11 +8,16 @@ from db import init_mysql
 from db import close_mysql
 
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="log.json",
+    format='%(asctime)s %(name)-14s %(levelname)-8s:: %(message)s',
+    )
 
-logging.basicConfig(level=logging.DEBUG)
+
 app = web.Application()
 app['config'] = config
 setup_routes(app)
 app.on_startup.append(init_mysql)
 app.on_cleanup.append(close_mysql)
-web.run_app(app)
+web.run_app(app, access_log_format="%s %r")
