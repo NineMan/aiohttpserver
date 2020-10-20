91eedad697c4a8ec35d441d1a9916aaf6922e7ca
NineMan <NineMan@github.com>
2020-10-20 10:16:39 +0400

    Split on layer

--- aiohttp_server/db.py
+++ aiohttp_server/db.py
@@ -3,6 +3,13 @@ import asyncio
 import aioredis
 
 
+# List fields of product in MySQL
+FIELDS = ["id", "product_name", "description", "value"]
+
+# Set of saved products in Redis (product_hash)
+NAME_OF_SET = 'products'
+
+
 async def init_mysql(app):
     config = app['config']['mysql']
     pool_mysql = await create_pool(
@@ -31,3 +38,62 @@ async def close_mysql(app):
 async def close_redis(app):
     app['db_redis'].close()
     await app['db_redis'].wait_closed()
+
+
+async def fetch_mysql(query, pool):
+    try:
+        async with pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute(query)
+                records = await cursor.fetchall()
+                return records
+    except Exception:
+        return False
+
+
+async def commit_mysql(query, pool):
+
+    try:
+        async with pool.acquire() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute(query)
+                await connection.commit()
+                return True
+
+    except Exception as e:
+        return False
+
+
+async def hashget_redis(pool):
+
+    try:
+        with await pool as connection:
+            product_hashes = await connection.execute("smembers", NAME_OF_SET)
+            response = []
+
+            for product_hash in product_hashes:
+                keys_values = await connection.execute("hgetall", product_hash)
+
+                resp = {}
+                for i in range(0, len(keys_values), 2):
+                    key = keys_values[i].decode("utf-8")
+                    val = keys_values[i + 1].decode("utf-8")
+                    resp[key] = val
+                resp["value"] = int(resp["value"])
+                response.append(resp)
+                return response
+    except Exception:
+        return False
+
+
+async def hashadd_redis(key, request, pool):
+
+    try:
+        with await pool as connection:
+            for field, value in request.items():
+                await connection.execute("hset", key, field, value)
+                await connection.execute("sadd", NAME_OF_SET, key)
+            return True
+
+    except Exception:
+        return False
--- aiohttp_server/views.py
+++ aiohttp_server/views.py
@@ -3,94 +3,10 @@ from aiofile import LineReader
 from aiohttp import web
 from json import dumps
 from json import loads
+from db import fetch_mysql, commit_mysql
+from db import hashget_redis, hashadd_redis
+from db import FIELDS
 
-
-# List fields of product in MySQL
-FIELDS = ["id", "product_name", "description", "value"]
-
-# Set of saved products in Redis (product_hash)
-NAME_OF_SET = 'products'
-
-
-async def fetch_mysql(query, pool):
-    try:
-        async with pool.acquire() as connection:
-            async with connection.cursor() as cursor:
-                await cursor.execute(query)
-                records = await cursor.fetchall()
-                return records
-    except Exception:
-        return False
-
-
-async def commit_mysql(query, pool):
-
-    try:
-        async with pool.acquire() as connection:
-            async with connection.cursor() as cursor:
-                await cursor.execute(query)
-                await connection.commit()
-                return True
-
-    except Exception as e:
-        return False
-
-
-async def write_log(request, response, file="log.json"):
-
-    try:
-        log = {
-            "status": response[1],
-            "method": request.method,
-            "path": request.path,
-            "data": response[0]
-        }
-        log = dumps(log) + "\n"
-
-        async with AIOFile(file, "a") as afp:
-            await afp.write(log)
-            await afp.fsync()
-        return True
-
-    except Exception:
-        return False
-
-
-async def hashget_redis(pool):
-
-    try:
-        with await pool as connection:
-            product_hashes = await connection.execute("smembers", NAME_OF_SET)
-            response = []
-
-            for product_hash in product_hashes:
-                keys_values = await connection.execute("hgetall", product_hash)
-
-                resp = {}
-                for i in range(0, len(keys_values), 2):
-                    key = keys_values[i].decode("utf-8")
-                    val = keys_values[i + 1].decode("utf-8")
-                    resp[key] = val
-                resp["value"] = int(resp["value"])
-                response.append(resp)
-                return response
-    except Exception:
-        return False
-
-
-async def hashadd_redis(key, request, pool):
-
-    try:
-        print("request =",request)
-        with await pool as connection:
-            for field, value in request.items():
-                print(field, value)
-                await connection.execute("hset", key, field, value)
-                await connection.execute("sadd", NAME_OF_SET, key)
-            return True
-
-    except Exception:
-        return False
 
 
 async def mysql_get_handler(request):
@@ -193,30 +109,9 @@ async def mysql_delete_handler(request):
     return web.json_response(resp_web[0], status=resp_web[1])
 
 
-async def log_get_handler(request):
-
-    try:
-        async with AIOFile("log.json", "r") as afp:
-            response = []
-            async for line in LineReader(afp):
-                string = loads(line)
-                response.append(string)
-
-        resp_log = "No data", 200
-        resp_web = response, 200
-        await write_log(request, resp_log)
-        return web.json_response(resp_web[0], status=resp_web[1])
-
-    except Exception:
-        resp_log = "No data", 400
-        resp_web = {"Error": "Log.json is not read"}, 400
-        await write_log(request, resp_log)
-        return web.json_response(resp_web[0], status=resp_web[1])
-
-
 async def redis_get_handler(request):
     pool_connection = request.app["db_redis"]
-    answer = await hget_redis(pool_connection)
+    answer = await hashget_redis(pool_connection)
 
     if answer:
         response = answer, 200
@@ -253,5 +148,46 @@ async def redis_post_handler(request):
 
         resp_log = request_json, 400
         resp_web = {"Error": "Redis HASHSET not executed"}, 400
+        await write_log(request, resp_log)
+        return web.json_response(resp_web[0], status=resp_web[1])
+
+
+async def write_log(request, response, file="log.json"):
+
+    try:
+        log = {
+            "status": response[1],
+            "method": request.method,
+            "path": request.path,
+            "data": response[0]
+        }
+        log = dumps(log) + "\n"
+
+        async with AIOFile(file, "a") as afp:
+            await afp.write(log)
+            await afp.fsync()
+        return True
+
+    except Exception:
+        return False
+
+
+async def log_get_handler(request):
+
+    try:
+        async with AIOFile("log.json", "r") as afp:
+            response = []
+            async for line in LineReader(afp):
+                string = loads(line)
+                response.append(string)
+
+        resp_log = "No data", 200
+        resp_web = response, 200
+        await write_log(request, resp_log)
+        return web.json_response(resp_web[0], status=resp_web[1])
+
+    except Exception:
+        resp_log = "No data", 400
+        resp_web = {"Error": "Log.json is not read"}, 400
         await write_log(request, resp_log)
         return web.json_response(resp_web[0], status=resp_web[1])
